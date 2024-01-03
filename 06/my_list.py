ff4038af63664314df82bc8399a764a21a8b885d
Lyubomir Dimitrov <lyubomir.dimitrov.8702@gmail.com>
2024-01-03 12:38:01 +0200

    add files

---
+++ 06/my_list.py
@@ -0,0 +1,113 @@
+from random import choice
+this_class = []
+class_members = int(input("Class Members: "))
+for i in range(1, class_members +1):
+    this_class.append(i)
+print(f"The choice one is {choice(this_class)}")
+
+
+# from datetime import datetime
+# from collections import deque
+# ml = []
+# num = int(input())
+# for i in range(num):
+#     ml.append(int(input()))
+# print(6 / sum(ml) * 100)
+# my_list = [ 2, 3, 4, 5, 6, 7, 8, 9]
+# my_queue = deque()
+# for i in range(1, 2_000_000):
+#     my_list.append(i)
+#     my_queue.append(i)
+#
+# start_1 = datetime.now()
+# for i in range(1, 10_000):
+#     my_list.pop(0)
+# end_1 = datetime.now()
+# print(end_1 - start_1)
+#
+# start_2 = datetime.now()
+# for i in range(1, 10_000):
+#     my_queue.popleft()
+# end_2 = datetime.now()
+# print(end_2 - start_2)
+
+
+#
+# my_queue = deque()
+# print(my_queue)
+# my_queue.append(1)
+# print(my_queue)
+# my_queue.append(2)
+# print(my_queue)
+# my_queue.append(3)
+# print(my_queue)
+# my_queue.append(13)
+# print(my_queue)
+# a = my_queue.popleft()
+# a += 10
+# print(a)
+# my_queue.pop()
+# print(my_queue)
+
+
+
+
+# print(my_list)
+# my_list.pop()
+# print(my_list)
+#
+# my_list.append("СФДФГДФГ")
+# print(my_list)
+
+
+
+# my_list.insert(5, "gosho")
+# print(my_list)
+#
+
+
+
+
+# my_list_1 = my_list.copy()
+# my_list_1.clear()
+# print(my_list)
+
+
+
+
+# my_list.extend(my_list_1)
+
+
+# print(sorted(my_list)[::-1])
+# my_list.sort()
+
+
+
+# print(my_list.index(2, 5))
+
+# txt = "aasaasaas"
+# print(my_list.count(2))
+
+
+# print(my_list.pop(0))
+
+# print(my_list)
+# my_list.pop()
+# print(my_list)
+# my_list.pop()
+
+# print(my_list[-12])
+# print(my_list[0])
+# my_list[4] , my_list[5] = my_list[5], my_list[4]
+
+
+# ml = my_list[5:8]
+#
+# print(ml)
+# my_list.reverse()
+
+
+
+# my_list.remove()
+# my_list.append(4)
+
