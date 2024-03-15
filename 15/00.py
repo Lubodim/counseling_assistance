2d1b21a6055a75307d9bd4cb94e9a33242ba34ce
Lyubomir Dimitrov <lyubomir.dimitrov.8702@gmail.com>
2024-03-15 09:40:11 +0200

    add files

---
+++ 15/00.py
@@ -0,0 +1,19 @@
+# a = input().split("-")
+#
+# int_a = []
+#
+# for i in a:
+#     int_a.append(int(i))
+# print(int_a)
+# print([int(x) for x in input().split("-")])
+# print(list(map(int, input().split("-"))))
+#
+# my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#
+# print(my_list[2])
+#
+# my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#
+# my_list[2], my_list[-1] = my_list[-1], my_list[2]
+
+print(my_list)
